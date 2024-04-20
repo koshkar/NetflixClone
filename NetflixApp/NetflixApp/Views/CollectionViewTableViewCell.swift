a9b312ffc578d20842676b6cdfcb9b3127227023
koshkar <qwerty376l@mail.ru>
2024-04-20 22:33:54 +0300

    Start work on custom cells + integrate into Home view

---
+++ NetflixApp/NetflixApp/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewTableViewCell.swift
+//  NetflixApp
+//
+//  Created by Кирилл Кошкарёв on 20.04.2024.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    static let identifier = "CollectionViewTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemPink
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+}
